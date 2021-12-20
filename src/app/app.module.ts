d637f0f2dcc753bfa5eaf28cc508b50f3d83c716
Chandra1215 <umeshchandra1215@gmail.com>
2021-12-20 16:11:12 +0530

    Ui folder structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
+import { HouseholdsComponent } from './households/households.component';
+import { MobilesComponent } from './mobiles/mobiles.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { CartComponent } from './cart/cart.component';
+import { GroceryComponent } from './grocery/grocery.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SigninComponent,
+    ElectronicsComponent,
+    HouseholdsComponent,
+    MobilesComponent,
+    FashionComponent,
+    CartComponent,
+    GroceryComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
